ec4191ca8bb96d3ea7e31767fe4ffe7148e4e40f
cherry <cherrycse.778@gmail.com>
2018-07-15 03:39:04 +0600

    My Grocery List 3

---
+++ app/src/main/java/com/example/personal/mygrocerylist/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.personal.mygrocerylist;
+
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayAdapter<String> adapter;
+    EditText editText;
+    ArrayList<String> itemList;
+
+
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+
+
+
+        String[] items={" "};
+        itemList=new ArrayList<String>(Arrays.asList(items));
+        adapter=new ArrayAdapter<String>(this,R.layout.list_item,R.id.txtview,itemList);
+        ListView listV=(ListView)findViewById(R.id.list);
+        listV.setAdapter(adapter);
+        editText=(EditText)findViewById(R.id.txtInput);
+        Button btAdd=(Button)findViewById(R.id.btAdd);
+        btAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newItem=editText.getText().toString();
+                // add new item to arraylist
+                itemList.add(newItem);
+                // notify listview of data changed
+
+                adapter.notifyDataSetChanged();
+            }
+
+        });
+
+    }
+
+
+
+}
+
